737b2e68e97f8da641a8c666d20ca02ca3db53f0
一心炼银 <83876165+tensorfly-gpu@users.noreply.github.com>
2023-02-26 16:55:30 +0800

    update environment

--- launch.py
+++ launch.py
@@ -1,7 +1,25 @@
 # 环境配置，启动ui界面
+import os
 
 def prepare_environment():
-    pass
+    # 安装环境
+    diffusers_auto_update()
+
+def diffusers_auto_update():
+    try:
+        import safetensors
+        from ppdiffusers.utils import image_grid
+        from paddlenlp.transformers.clip.feature_extraction import CLIPFeatureExtractor
+        from paddlenlp.transformers import FeatureExtractionMixin
+        print("environment all ready!")
+
+    except (ModuleNotFoundError, ImportError, AttributeError):
+        print('检测到库不完整, 正在安装库')
+        os.system("pip install -U pip  -i https://mirror.baidu.com/pypi/simple")
+        os.system("pip install -U OmegaConf --user -i https://mirror.baidu.com/pypi/simple")
+        os.system("pip install ppdiffusers==0.9.0 --user -i https://mirror.baidu.com/pypi/simple")
+        os.system("pip install paddlenlp==2.4.9 --user -i https://mirror.baidu.com/pypi/simple")
+        os.system("pip install -U safetensors --user -i https://mirror.baidu.com/pypi/simple")
 
 def start():
     pass
